ee19e4859ceef616af1c1dc724f99bedd093569e
kingteng20 <james.abarca@student.ateneo.edu>
2024-03-03 10:39:38 +0800

    added models and updated the views and templates

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import index, recipes_list, recipe_1, recipe_2
+from .views import RecipeListView, RecipeDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('recipes/list', recipes_list, name='recipes_list'),
-    path('recipe/1', recipe_1, name='recipe_1'),
-    path('recipe/2', recipe_2, name='recipe_2'),
+    path('recipes/list', RecipeListView.as_view(), name='recipes-list'),
+    path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-details')
 ]
 
 app_name = 'ledger'
--- ledger/views.py
+++ ledger/views.py
@@ -1,138 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Recipe
 
-def index(request):
-    return HttpResponse("landing page")
 
 def recipes_list(request):
-    ctx = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
-}
+    recipes = Recipe.objects.all()
+    ctx = { "recipes": recipes }
     return render(request, 'recipes_list.html', ctx)
 
-def recipe_1(request):
-    ctx = {
-        "name": "Recipe 1",
-        "ingredients": [
-            {
-                "name": "tomato",
-                "quantity": "3pcs"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "pork",
-                "quantity": "1kg"
-            },
-            {
-                "name": "water",
-                "quantity": "1L"
-            },
-            {
-                "name": "sinigang mix",
-                "quantity": "1 packet"
-            }
-        ],
-        "link": "/recipe/1"
-    }
+
+def recipe_details(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    ctx = { "recipe": recipe }
     return render(request, 'recipe.html', ctx)
 
-def recipe_2(request):
-    ctx = {
-        "name": "Recipe 2",
-        "ingredients": [
-            {
-                "name": "garlic",
-                "quantity": "1 head"
-            },
-            {
-                "name": "onion",
-                "quantity": "1pc"
-            },
-            {
-                "name": "vinegar",
-                "quantity": "1/2cup"
-            },
-            {
-                "name": "water",
-                "quantity": "1 cup"
-            },
-            {
-                "name": "salt",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "whole black peppers",
-                "quantity": "1 tablespoon"
-            },
-            {
-                "name": "pork",
-                "quantity": "1 kilo"
-            }
-        ],
-        "link": "/recipe/2"
-    }
-    return render(request, 'recipe.html', ctx)
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes_list.html'
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe.html'
