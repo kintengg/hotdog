3e088764e49b65c192a4d2fefcc519037f950de5
kingteng20 <james.abarca@student.ateneo.edu>
2024-03-15 21:00:13 +0800

    created an accounts app with custom user model called Profile, and included a custom login flow with password reset

--- ledger/views.py
+++ ledger/views.py
@@ -1,20 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+
 from .models import Recipe
-
-
-def recipes_list(request):
-    recipes = Recipe.objects.all()
-    ctx = { "recipes": recipes }
-    return render(request, 'recipes_list.html', ctx)
-
-
-def recipe_details(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
-    ctx = { "recipe": recipe }
-    return render(request, 'recipe.html', ctx)
 
 
 class RecipeListView(ListView):
@@ -22,6 +10,6 @@ class RecipeListView(ListView):
     template_name = 'recipes_list.html'
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe.html'
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ledger.urls', namespace='ledger'))
+    path('', include('ledger.urls', namespace='ledger')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
